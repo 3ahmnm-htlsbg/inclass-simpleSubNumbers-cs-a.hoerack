b46603d8d20f58b9a237d2bafdae29b568e14d61
ahoerack03 <anna.hoerack2003@gmail.com>
2019-12-18 12:33:00 +0100

    create and attached script, program can subtract numbers and show the result, reset button to reset the program and type in new numbers, reset button cannot be clicked when no numbers are in the inputfields, subnumbers cannot be clicked when it's not resetted

---
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputeNumbers : MonoBehaviour
+{
+    public Text result;
+    public InputField varA;
+     public InputField varB;
+     public Button btn_reset;
+     public Button btn_subNumbers;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private int SubNumbers()
+    {
+        int a = int.Parse(varA.text);
+        int b = int.Parse(varB.text);
+        return a - b;
+    }
+    public void SetResult()
+    {
+        result.text = SubNumbers().ToString();
+        varA.interactable = false;
+        varB.interactable = false;
+        btn_subNumbers.interactable = false;
+        btn_reset.interactable = true;
+    }
+    public void ResetGui()
+
+    {
+        result.text = "Result";
+        varA.text = "0";
+        varB.text = "0";
+        varA.interactable = true;
+        varB.interactable = true;
+        btn_subNumbers.interactable = true;
+        btn_reset.interactable = false;
+    }
+}
